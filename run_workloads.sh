5ee83587dd7ccab21cb6efeaf4e1be68650b4d49
Iwanowski, Maciej <maciej.iwanowski@intel.com>
2018-11-06 14:59:54 -0800

    Adding license headers to some omitted files (#147)

--- run_workloads.sh
+++ run_workloads.sh
@@ -1,5 +1,19 @@
 #!/bin/bash -ex
 
+# Copyright (c) 2018 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # Run multiple configurations files run like this:
 #ls -1 config-*.sh | xargs -n1 -P 8 bash run_workloads.sh 
 
