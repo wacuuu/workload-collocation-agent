0fb871858cc65301ab85e2b0c6a777d86f1f4dee
Pawel Palucki <pawel.palucki@intel.com>
2019-07-16 15:48:46 +0200

    split mutilate && memcached

--- wca/extra/k8s_extender.py
+++ wca/extra/k8s_extender.py
@@ -4,45 +4,114 @@ import logging
 import socketserver
 from typing import Dict, List
 
+import requests
 from dataclasses import dataclass
 
 log = logging.getLogger(__name__)
 
+# PUBLIC CONFIG
+PORT = 12345
+PROMETHEUS = 'http://100.64.176.36:9090'
+NAMESPACE = 'default'
+LEVEL = logging.INFO
+
+# CONSTANTS
 _PROMETHEUS_QUERY_PATH = "/api/v1/query"
 _PROMETHEUS_QUERY_RANGE_PATH = "/api/v1/query_range"
 _PROMETHEUS_URL_TPL = '{prometheus}{path}?query={name}'
 _PROMETHEUS_TIME_TPL = '&start={start}&end={end}&step=1s'
 _PROMETHEUS_TAG_TPL = '{key}="{value}"'
 
-def _build_prometheus_url(prometheus, name, tags=None, window_size=None, event_time=None):
-    tags = tags or dict()
+
+def _build_raw_query(prometheus, query):
     path = _PROMETHEUS_QUERY_PATH
-    time_range = ''
+    url = _PROMETHEUS_URL_TPL.format(
+        prometheus=prometheus,
+        path=path,
+        name=query,
+    )
+    return url
+
+
+def _build_prometheus_url(prometheus, name, tags=None, start=None, end=None):
+    tags = tags or dict()
 
     # Some variables need to be overwritten for range queries.
-    if window_size and event_time:
-        offset = window_size / 2
-        time_range = _PROMETHEUS_TIME_TPL.format(
-            start=event_time - offset,
-            end=event_time + offset)
+    if start and end:
         path = _PROMETHEUS_QUERY_RANGE_PATH
+        time_range = _PROMETHEUS_TIME_TPL.format(start=start, end=end)
+    else:
+        path = _PROMETHEUS_QUERY_PATH
+        time_range = ''
 
-    url = _PROMETHEUS_URL_TPL.format(
+    url_and_query_without_tags = _PROMETHEUS_URL_TPL.format(
         prometheus=prometheus,
         path=path,
         name=name,
     )
 
-    # Prepare additional tags for query.
-    query_tags = []
-    for k, v in tags.items():
-        query_tags.append(_PROMETHEUS_TAG_TPL.format(key=k, value=v))
-    query_tags_str = ','.join(query_tags)
-
     # Build final URL from all the components.
-    url = ''.join([url, "{", query_tags_str, "}", time_range])
+    if tags:
+        # Prepare additional tags for query.
+        query_tags = []
+        for k, v in tags.items():
+            query_tags.append(_PROMETHEUS_TAG_TPL.format(key=k, value=v))
+        query_tags_str = ','.join(query_tags)
+        url = ''.join([url_and_query_without_tags, "{", query_tags_str, "}", time_range])
+    else:
+        url = ''.join([url_and_query_without_tags, time_range])
 
     return url
+
+
+def do_query(name, result_tag, tags=None):
+    url = _build_prometheus_url(PROMETHEUS, name, tags)
+    response = requests.get(url)
+    response = response.json()
+    if response['status'] == 'error':
+        raise Exception(response['error'])
+    assert response['data']['resultType'] == 'vector'
+    result = response['data']['result']
+    return {pair['metric'][result_tag]: float(pair['value'][1]) for pair in result}
+
+
+def do_raw_query(query, result_tag):
+    url = _build_raw_query(PROMETHEUS, query)
+    response = requests.get(url)
+    response = response.json()
+    if response['status'] == 'error':
+        raise Exception(response['error'])
+    assert response['data']['resultType'] == 'vector'
+    result = response['data']['result']
+    return {pair['metric'][result_tag]: float(pair['value'][1]) for pair in result}
+
+
+def _filter_logic(app, nodes, namespace):
+    return nodes
+
+
+def _prioritize_logic(app, nodes, namespace):
+    if namespace != NAMESPACE:
+        log.info('ignoring pods not from %r namespace (got %r)', NAMESPACE, namespace)
+        return {}
+
+    cache_contention_by_node = do_query('node__cache_contention', 'node')
+    if app is None:
+        log.info('app is unknown - missing label app - prioritize by node_cache_contetion only')
+        priorities = cache_contention_by_node
+    else:
+        # Get most fitted nodes.
+        priorities = {}
+        query = '1 / abs(node__cache_utilization_class - on() group_left app__cache_utilization{app="%s"})' % app
+        nodes_fit = do_raw_query(query, 'node')
+        if not nodes_fit:
+            # Unknown app - first seen.
+            priorities = cache_contention_by_node
+        else:
+            log.info('nodes_fit for %r: %r', app, nodes_fit)
+            for node in nodes:
+                priorities[node] = int(nodes_fit.get(node, 0) * 100)
+    return priorities
 
 
 @dataclass
@@ -50,16 +119,6 @@ class ExtenderArgs:
     Nodes: List[Dict]
     Pod: dict
     NodeNames: List[str]
-
-
-# ExtenderFilterResult represents the results of a filter call to an extender
-@dataclass
-class ExtenderFilterResult:
-    NodeNames: List[str]
-    # Filtered out nodes where the pod can't be scheduled and the failure messages
-    FailedNodes: Dict[str, str]
-    # Error message indicating failure
-    Error: str
 
 
 class K8SHandler(http.server.BaseHTTPRequestHandler):
@@ -82,52 +141,58 @@ class K8SHandler(http.server.BaseHTTPRequestHandler):
         self.end_headers()
 
         result_as_json = json.dumps(result_simple).encode()
-        #log.debug('%s %s', self.path, result_as_json)
+        log.debug('%s %s', self.path, result_as_json)
         self.wfile.write(result_as_json)
 
     def log_request(self, code='-', size='-'):
         pass
 
-    def _filter(self, extender_args):
+    def _extract_common_input(self, extender_args):
+        """ :returns app, nodes, namespace"""
         nodes = extender_args.NodeNames
-        # nodes = list(sorted(extender_args.NodeNames[0:3]))
+        labels = extender_args.Pod['metadata']['labels']
+        # generic input
+        namespace = extender_args.Pod['metadata']['namespace']
+        # specific workloads input
+        app = labels.get('app', None)
+        return app, nodes, namespace
+
+    def _filter(self, extender_args):
+        # Logic
+        log.debug('Pod: \n%s:', json.dumps(extender_args.Pod, indent=4))
+        app, nodes, namespace = self._extract_common_input(extender_args)
+        self._extract_common_input(extender_args)
+        nodes = _filter_logic(app, nodes, namespace)
         log.info('filtered = %s', ', '.join(nodes))
+        # Encode
         return dict(
             NodeNames=nodes,
-            # NodeNames=extender_args.NodeNames,
-            # NodeNames=[],
             FailedNodes={},
             Error='',
         )
 
     def _prioritize(self, extender_args: ExtenderArgs):
-        print(json.dumps(extender_args.Pod, indent=4))
-        labels = extender_args.Pod['metadata']['labels']
-
-
-        nodes = []
-        for score, nodename in enumerate(extender_args.NodeNames):
-            nodes.append(
-                dict(Host=nodename, Score=0)
-            )
-
-        log.info('priorities = %s', ', '.join('%s=%s' % (d['Host'], d['Score']) for d in sorted(nodes, key=lambda d:d['Host'])))
-        return nodes
+        # Logic
+        app, nodes, namespace = self._extract_common_input(extender_args)
+        priorities = _prioritize_logic(app, nodes, namespace)
+        log.info('priorities: %r', priorities)
+        # Encode as PriorityList
+        priority_list = [dict(Host=node, Score=priorities.get(node, 0)) for node in nodes]
+        log.debug('priority list = %s', ', '.join('%s=%s' % (d['Host'], d['Score']) for d in
+                                                  sorted(priority_list, key=lambda d: d['Host'])))
+        return priority_list
 
 
 class ThreadingHTTPServer(socketserver.ThreadingMixIn, http.server.HTTPServer):
     daemon_threads = True
 
 
-def start_server(port):
+def main(level, port):
+    logging.basicConfig(level=level)
     with http.server.HTTPServer(('', port), K8SHandler) as httpd:
         log.info('serving at port: %s', port)
         httpd.serve_forever()
 
-# STATE of cluster and latest characteristics of the apps
-app_stats = {}
-node_stats = {}
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG)
-    start_server(12345)
+    main(LEVEL, PORT)
