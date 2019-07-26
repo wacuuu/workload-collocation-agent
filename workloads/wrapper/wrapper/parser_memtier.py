9e232252be58a83c96ef25a24a14a548fc65ad6c
Norbert Szulc <norbert.szulc@intel.com>
2019-07-26 16:03:18 +0200

    Change wrapper to read from file

--- workloads/wrapper/wrapper/parser_memtier.py
+++ workloads/wrapper/wrapper/parser_memtier.py
@@ -11,32 +11,42 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+import json
+import time
+import logging
 from io import TextIOWrapper
 from typing import List
-import json
+
 from wca.metrics import Metric, MetricType
 from wrapper import wrapper_main
 
-EOF_line = "Stop-Mutilate-Now"
+log = logging.getLogger(__name__)
 
+EOF_line = "Stop-Memtier-Now"
 
 def parse(input: TextIOWrapper, regexp: str, separator: str = None,
           labels=None, metric_name_prefix: str = '') -> List[Metric]:
     """Custom parse function for memtier_benchmark
-    memtier return json if properly configured, it's not 100% compliant but python json should
-    be able to take care of it
+    memtier can't return proper json to stdout, tmp file is needed
     """
     if labels is None:
         labels = {}
     new_metrics = []
 
-    json_data = json.load(input)
+    # HACK: wait for process to finish in order to read from json file
+    # _ = input.read()
+    time.sleep(1)
+    with open("/tmp/memtier_benchmark") as f:
+        try:
+            json_data = json.load(f)
+        except json.decoder.JSONDecodeError as e:
+            log.warning("Can't parse file")
+            return []
 
     all_stats = json_data["ALL STATS"]
 
     new_metrics.append(Metric(
-        metric_name_prefix + 'qps', all_stats["Total"]["Ops/sec"],
+        metric_name_prefix + 'qps', all_stats["Totals"]["Ops/sec"],
         type=MetricType.GAUGE, labels=labels, help="QPS"
     ))
 
