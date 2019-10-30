a53d2c01cedd3c6781c51e9eafe1da1daa420af7
Damian Darczuk <damian.darczuk@intel.com>
2019-10-30 12:01:00 +0100

    WIP E2E tests Daemonset WCA (#287)
    * Merge patches stress-ng. Metrics for counter and diff
    
    * Test new stress-ng parser on k8s
    
    * Test e2e stress-ng
    
    * Split Kubernetes dashboard

--- workloads/wrapper/wrapper/parser_stress_ng.py
+++ workloads/wrapper/wrapper/parser_stress_ng.py
@@ -25,66 +25,72 @@ EOF_line = "..."
 def parse(input: TextIOWrapper, regexp: str, separator: str = None,
           labels: Dict[str, str] = {}, metric_name_prefix: str = '') -> List[Metric]:
     """Custom parse function for stress_ng
-        stress-ng: info:  [99] Time 1546433449, counter=173
-        stress-ng: info:  [96] Time 1546433449, counter=210
-        stress-ng: info:  [103] Time 1546433449, counter=191
-        stress-ng: info:  [104] Time 1546433449, counter=195
-        stress-ng: info:  [106] Time 1546433449, counter=197
-        stress-ng: info:  [101] Time 1546433450, counter=250
-        stress-ng: info:  [98] Time 1546433450, counter=261
-        stress-ng: info:  [97] Time 1546433450, counter=273
-        stress-ng: info:  [99] Time 1546433450, counter=217
-        stress-ng: info:  [96] Time 1546433450, counter=263
-        stress-ng: info:  [113] stress-ng-stream: memory rate: 1806.87 MB/sec, 722.75 Mflop/sec
-        stress-ng: info:  [114] Time 1546433537, counter=304
-        stress-ng: info:  [115] Time 1546433537, counter=282
-        stress-ng: info:  [119] stress-ng-stream: memory rate: 1742.69 MB/sec, 697.07 Mflop/sec
-        stress-ng: info:  [117] stress-ng-stream: memory rate: 1999.33 MB/sec, 799.73 Mflop/sec
-        stress-ng: info:  [115] stress-ng-stream: memory rate: 1922.38 MB/sec, 768.95 Mflop/sec
-        stress-ng: info:  [114] stress-ng-stream: memory rate: 2067.34 MB/sec, 826.94 Mflop/sec
-        stress-ng: info:  [121] stress-ng-stream: memory rate: 1849.08 MB/sec, 739.63 Mflop/sec
-        stress-ng: info:  [123] stress-ng-stream: memory rate: 1848.92 MB/sec, 739.57 Mflop/sec
-        stress-ng: info:  [116] stress-ng-stream: memory rate: 2027.03 MB/sec, 810.81 Mflop/sec
-        stress-ng: info:  [112] successful run completed in 6.02s
+        stress-ng: info:  [25] Time 1572363779, counter 425, diff 61
+        stress-ng: info:  [27] Time 1572363779, counter 426, diff 61
+        stress-ng: info:  [23] Time 1572363780, counter 475, diff 60
+        stress-ng: info:  [26] Time 1572363780, counter 322, diff 40
+        stress-ng: info:  [24] Time 1572363780, counter 321, diff 39
+        stress-ng: info:  [25] Time 1572363780, counter 485, diff 60
+        stress-ng: info:  [27] Time 1572363780, counter 485, diff 59
+        stress-ng: info:  [23] Time 1572363781, counter 536, diff 61
+        stress-ng: info:  [24] Time 1572363781, counter 362, diff 41
+        stress-ng: info:  [26] Time 1572363781, counter 363, diff 41
+        stress-ng: info:  [25] Time 1572363781, counter 547, diff 62
+        stress-ng: info:  [27] Time 1572363781, counter 547, diff 62
+        stress-ng: info:  [23] stress-ng-stream: memory rate: 3852.67 MB/sec, 1541.07 Mflop/sec /
+        (instance 0)
+        stress-ng: info:  [27] stress-ng-stream: memory rate: 3922.76 MB/sec, 1569.10 Mflop/sec /
+        (instance 4)
+        stress-ng: info:  [26] Time 1572363782, counter 403, diff 40
+        stress-ng: info:  [24] Time 1572363782, counter 402, diff 40
+        stress-ng: info:  [25] Time 1572363782, counter 608, diff 61
+        stress-ng: info:  [27] Time 1572363782, counter 609, diff 62
+        stress-ng: info:  [25] stress-ng-stream: memory rate: 3920.62 MB/sec, 1568.25 Mflop/sec /
+        (instance 2)
+        stress-ng: info:  [24] stress-ng-stream: memory rate: 2597.47 MB/sec, 1038.99 Mflop/sec /
+        (instance 1)
+        stress-ng: info:  [26] stress-ng-stream: memory rate: 2603.78 MB/sec, 1041.51 Mflop/sec /
+        (instance 3)
+        stress-ng: info:  [22] successful run completed in 10.01s
         ---
         system-info:
-              stress-ng-version: 0.09.28
+              stress-ng-version: 0.10.08
               run-by: root
-              date-yyyy-mm-dd: 2019:01:02
-              time-hh-mm-ss: 12:52:17
-              epoch-secs: 1546433537
-              hostname: d4840a594b43
+              date-yyyy-mm-dd: 2019:10:29
+              time-hh-mm-ss: 15:43:02
+              epoch-secs: 1572363782
+              hostname: 31ff53b6528c
               sysname: Linux
-              nodename: d4840a594b43
-              release: 4.15.0-43-generic
-              version: #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018
+              nodename: 31ff53b6528c
+              release: 4.15.0-66-generic
+              version: #75-Ubuntu SMP Tue Oct 1 05:24:09 UTC 2019
               machine: x86_64
-              uptime: 6933
-              totalram: 33605246976
-              freeram: 10913120256
-              sharedram: 1651642368
-              bufferram: 649097216
-              totalswap: 34233905152
-              freeswap: 34233905152
+              uptime: 28799
+              totalram: 33605263360
+              freeram: 14022410240
+              sharedram: 2681659392
+              bufferram: 1237618688
+              totalswap: 0
+              freeswap: 0
               pagesize: 4096
               cpus: 8
               cpus-online: 8
               ticks-per-second: 100
 
-        stress-ng: info:  [112] stressor       bogo ops real time  usr time  sys time   bogo ops/s /
-        bogo ops/s
-        stress-ng: info:  [112]                           (secs)    (secs)    (secs)   (real time) /
+        stress-ng: info:  [22] stressor       bogo ops real time  usr time  sys time   bogo ops/s  /
+         bogo ops/s
+        stress-ng: info:  [22]                           (secs)    (secs)    (secs)   (real time) /
         (usr+sys time)
         metrics:
-        stress-ng: info:  [112] stream             2250      6.01     40.81      0.39       374.12 /
-        54.61
+        stress-ng: info:  [22] stream             2623     10.00     49.82      0.10       262.18  /
+        52.54
             - stressor: stream
-              bogo-ops: 2250
-              bogo-ops-per-second-usr-sys-time: 54.611650
-              bogo-ops-per-second-real-time: 374.121510
-              wall-clock-time: 6.014089
-              user-time: 40.810000
-              system-time: 0.390000
+              bogo-ops: 2623
+              bogo-ops-per-second-usr-sys-time: 52.544071
+              bogo-ops-per-second-real-time: 262.179501
+              wall-clock-time: 10.004596
+              user-time: 49.820000
+              system-time: 0.100000
 
         ...
     """
@@ -135,18 +141,24 @@ def parse(input: TextIOWrapper, regexp: str, separator: str = None,
                    help="Summary bogo ops/s usr+sys time"))
 
     info = re.search(r'stress-ng: info: {2}\[(?P<id>\d*)\]+ ' +
-                     r'Time (?P<time>\d*), counter=(?P<counter>\d*)', new_line)
+                     r'Time (?P<time>\d*), counter (?P<counter>\d*), diff (?P<diff>\d*)', new_line)
 
     if info is not None:
+        time = info['time']
         id_proc = info['id']
         counter = int(info['counter'])
+        diff = int(info['diff'])
 
         labels.update({"id_proc_stress_ng": id_proc})
-
+        labels.update({"stress_ng_time": time})
         new_metrics.append(
             Metric(metric_name_prefix + 'bogo_ops_counter', counter,
                    type=MetricType.COUNTER, labels=labels,
                    help="Counter bogo ops per proc stress-ng, updated per 1 sec"))
+        new_metrics.append(
+            Metric(metric_name_prefix + 'bogo_ops_gauge', diff,
+                   type=MetricType.GAUGE, labels=labels,
+                   help="Gauge bogo ops per proc stress-ng, updated per 1 sec"))
 
     return new_metrics
 
