78a0a741637fe819a5f7e9b5b5e97d089b58c931
Damian Darczuk <damian.darczuk@intel.com>
2019-11-21 12:27:35 +0100

    Removed files (#309)

--- run_dev.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Only for development purposes!"
-cd "$(dirname "$0")"
-CONFIG=${CONFIG:-configs/extra/static_measurements.yaml}
-python3 -mpipenv run sudo env PYTHONPATH=. `python3 -mpipenv --py` wca/main.py -c $PWD/$CONFIG --root
