c4943b61a59713b3234e347cba3cebebe1e79bb7
Paweł Szulik <pawel.szulik@intel.com>
2020-01-22 11:18:54 +0100

    SSL for prometheus

--- wca/scheduler/data_providers/prometheus_data_provider.py
+++ wca/scheduler/data_providers/prometheus_data_provider.py
@@ -1,10 +1,24 @@
+# Copyright (c) 2020 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 import requests
 from dataclasses import dataclass
-from typing import List, Dict
+from typing import List, Dict, Optional
 
+from wca.config import Numeric
 from wca.scheduler.data_providers import DataProvider
 from wca.scheduler.types import ResourceType
-from wca.security import SSL
+from wca.security import SSL, HTTPSAdapter
 
 QUERY_PATH = "/api/v1/query"
 URL_TPL = '{prometheus_ip}{path}?query={name}'
@@ -41,12 +55,8 @@ APP_REQUESTED_RESOURCES_QUERY_MAP: Dict[ResourceType, str] = {
 @dataclass
 class PrometheusDataProvider(DataProvider):
     ip: str
-    ssl: SSL = None
-
-    def __post_init__(self):
-        self.session = requests.Session()
-        prometheus_adapter = requests.adapters.HTTPAdapter(max_retries=1)
-        self.session.mount(self.ip, prometheus_adapter)
+    timeout: Optional[Numeric(1, 60)] = 1.0
+    ssl: Optional[SSL] = None
 
     def get_node_free_resources(
             self, resources: List[ResourceType]) -> Dict[str, Dict[ResourceType, float]]:
@@ -84,8 +94,17 @@ class PrometheusDataProvider(DataProvider):
                 path=QUERY_PATH,
                 name=query)
         try:
-            response = self.session.get(url, timeout=1)
-            response.raise_for_status()
+            if self.ssl:
+                s = requests.Session()
+                s.mount(self.ip, HTTPSAdapter())
+                response = s.get(
+                        url,
+                        timeout=self.timeout,
+                        verify=self.ssl.server_verify,
+                        cert=self.ssl.get_client_certs())
+            else:
+                response = requests.get(url, timeout=self.timeout)
+                response.raise_for_status()
         except requests.exceptions.RequestException as e:
             raise PrometheusDataProviderException(e)
 
