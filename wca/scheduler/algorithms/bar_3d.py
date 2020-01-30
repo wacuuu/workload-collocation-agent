458d00751ce57013a2383d941743e040bdb42b8c
Szymon Bugaj <felidadae@gmail.com>
2020-01-30 11:25:24 +0100

    Merge 3D BAR with Generic BAR.

--- wca/scheduler/algorithms/bar_3d.py
+++ wca/scheduler/algorithms/bar_3d.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 from typing import List, Tuple, Union
 
 from wca.metrics import Metric
@@ -35,16 +36,15 @@ class Bar3D(Algorithm):
         requested = {
                 ResourceType.CPU: 2,
                 ResourceType.MEM: 10000,
-                ResourceType.MEMORY_BANDWIDTH_READS: 10000,
-                ResourceType.MEMORY_BANDWIDTH_WRITES: 10000
+                ResourceType.MEMBW_READ: 10000,
+                ResourceType.MEMBW_WRITE: 10000
                 }
-
         # TODO: Replace with real data from provider.
         capacity = {
                 ResourceType.CPU: 4,
                 ResourceType.MEM: 20000,
-                ResourceType.MEMORY_BANDWIDTH_READS: 20000,
-                ResourceType.MEMORY_BANDWIDTH_WRITES: 20000
+                ResourceType.MEMBW_READ: 20000,
+                ResourceType.MEMBW_WRITE: 20000
         }
 
         cpu_fraction = fraction_of_capacity(
@@ -52,20 +52,67 @@ class Bar3D(Algorithm):
         memory_fraction = fraction_of_capacity(
                 requested[ResourceType.MEM], capacity[ResourceType.MEM])
 
-        # TODO: Check if it's correct.
-        memory_bandwidth_requested = requested[ResourceType.MEMORY_BANDWIDTH_READS] + requested[ResourceType.MEMORY_BANDWIDTH_WRITES]
-
-        memory_bandwidth_capacity = capacity[ResourceType.MEMORY_BANDWIDTH_READS] + capacity[ResourceType.MEMORY_BANDWIDTH_WRITES]
+        memory_bandwidth_requested = requested[ResourceType.MEMBW_READ] + 4 * requested[ResourceType.MEMBW_WRITE]
+        memory_bandwidth_capacity = capacity[ResourceType.MEMBW_READ] + 4 * capacity[ResourceType.MEMBW_WRITE]
 
         memory_bandwidth_fraction = fraction_of_capacity(
                 memory_bandwidth_requested, memory_bandwidth_capacity)
 
         mean = cpu_fraction + memory_fraction + memory_bandwidth_fraction / 3
-        variance = ((cpu_fraction - mean) * (cpu_fraction - mean)) + ((memory_fraction - mean) * (memory_fraction - mean)) + ((memory_bandwidth_fraction - mean) 
-
         variance = pow((cpu_fraction-mean), 2) + pow((memory_fraction-mean), 2) + pow((memory_bandwidth_fraction - mean), 2)
         variance = variance / 3
 
+
+class BARGeneric(FFDGeneric):
+    def calculate_node_free_resources(self, capacity, used) -> Resources:
+        free = capacity.copy()
+        for dimension in self.dimensions:
+            if dimension not in (rt.MEMBW_READ, rt.MEMBW_WRITE):
+                free[dimension] -= used[dimension]
+        free[rt.MEMBW_READ] = capacity[rt.MEMBW_READ] - (used[rt.MEMBW_READ] + used[rt.MEMBW_WRITE] * 4)
+        free[rt.MEMBW_WRITE] = capacity[rt.MEMBW_WRITE] - (used[rt.MEMBW_WRITE] + used[rt.READ] / 4)
+        return free
+
+    def calculacte_app_requested_fraction(self, requested, free) -> Dict[ResourceType, float]:
+        """Flats MEMBW_WRITE and MEMBW_READ to single dimension MEMBW_FLAT"""
+        fractions = {}
+        for dimension in self.dimensions:
+            if dimension not in (rt.MEMBW_READ, rt.MEMBW_WRITE):
+                fractions[dimension] = float(requested[dimension]) / float(free[dimension]])
+        fractions[rt.MEMBW_FLAT] = (requested[rt.MEMBW_READ] + 4*requested[rt.MEMBW_WRITE]) / (free[rt.MEMBW_READ] + 4*free[rt.MEMBW_WRITE])
+        return fractions
+
+    def prioritize(self, extender_args: ExtenderArgs) -> List[HostPriority]:
+        nodes = sorted(extender_args.NodeNames)
+        log.info('[Prioritize] Nodes: %r' % nodes)
+
+        priorities = []
+
+        dp = self.data_provider
+        assigned_tasks = dp.get_assigned_tasks_requested_resources(self.dimensions, nodes)
+        node_capacities = dp.get_nodes_capacities(self.dimensions)
+        app_requested = dp.get_app_requested_resources(self.dimensions, app)
+        
+        log.debug('Iterating through nodes.')
+        for node in nodes:
+            if node not in node_capacities:
+                log.warning('Missing Node %r information!' % node)
+                extender_filter_result.NodeNames.append(node)
+                break
+
+            node_used_resources = self.used_resources_on_node(assigned_tasks[node])
+            node_free_resources = calculate_node_free_resources(capacity, used)  # allocable
+
+            app_requested_fraction = calculacte_app_requested_fraction()
+            mean = sum([v for v in app_requested_fraction.values()])/len(app_requested_fraction)
+            if len(app_requested_fraction) > 2:
+                variance = sum([(fraction - mean)*(fraction - mean) for fraction in app_requested_fraction]) / len(app_requested_fraction)
+            else:
+                variance = abs()
+            score = int((1-variance) * self.get_max_node_score())
+            priorities.append(HostPriority(node, score))
+
+        return priorities
 
 
 
