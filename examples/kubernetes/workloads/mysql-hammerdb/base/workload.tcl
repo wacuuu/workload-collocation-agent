8cafa823e8c585205030cccdfed157815e44a363
Damian Darczuk <damian.darczuk@intel.com>
2020-02-13 16:11:45 +0100

    Workload mysql-hammerdb stabilize (#405)

--- examples/kubernetes/workloads/mysql-hammerdb/base/workload.tcl
+++ examples/kubernetes/workloads/mysql-hammerdb/base/workload.tcl
@@ -1,4 +1,5 @@
 #!/bin/tclsh
+
 proc runtimer { seconds } {
 set x 0
 set timerstop 0
@@ -14,6 +15,8 @@ if {  [ vucomplete ] || $x eq $seconds } { set timerstop 1 }
     }
 return
 }
+
+
 dbset db mysql
 diset connection mysql_host MYSQL_HOST
 diset connection mysql_port 3306
@@ -21,12 +24,19 @@ diset tpcc mysql_user testuser
 diset tpcc mysql_pass testpassword
 diset tpcc mysql_driver timed
 diset tpcc mysql_rampup 1
-diset tpcc mysql_duration 60
+diset tpcc mysql_timeprofile true
+diset tpcc mysql_allwarehouse true
+diset tpcc mysql_duration 1209600000
+diset tpcc mysql_total_iterations 1000000000
 print dict
-
 loadscript
+
 vuset vu VIRTUAL_USERS
+vuset delay 500
+vuset repeat 1209600000
 vucreate
 vurun
-runtimer 60000
+
+runtimer 1209600000
+vudestroy
 after 5000
