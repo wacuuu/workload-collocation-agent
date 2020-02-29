2fe41200aa92ac9e1dbca3760fc325bd1eff90bd
Paweł Szulik <pawel.szulik@intel.com>
2020-02-29 18:21:33 +0100

    fix unit

--- tests/scheduler/data_providers/cluster_data_provider/test_cluster_data_provider.py
+++ tests/scheduler/data_providers/cluster_data_provider/test_cluster_data_provider.py
@@ -8,30 +8,6 @@ from wca.scheduler.data_providers.cluster_data_provider import (
         Queries)
 from wca.scheduler.types import ResourceType
 
-CLUSTER_CPU_MEM_CAPACITIES = {
-        'node200': {'cpu': 48.0, 'mem': 201},
-        'node201': {'cpu': 48.0, 'mem': 201},
-        'node202': {'cpu': 48.0, 'mem': 201},
-        'node203': {'cpu': 48.0, 'mem': 201},
-        'node36': {'cpu': 72.0, 'mem': 404},
-        'node37': {'cpu': 40.0, 'mem': 1064},
-        'node38': {'cpu': 40.0, 'mem': 404},
-        'node39': {'cpu': 40.0, 'mem': 404},
-        'node40': {'cpu': 40.0, 'mem': 1065}
-}
-
-CLUSTER_MEMBW_CAPACITIES = {
-        'node200': {'membw_read': 255999960000, 'membw_write': 255999960000},
-        'node201': {'membw_read': 255999960000, 'membw_write': 255999960000},
-        'node202': {'membw_read': 255999960000, 'membw_write': 255999960000},
-        'node203': {'membw_read': 255999960000, 'membw_write': 255999960000},
-        'node36': {},
-        'node37': {'membw_read': 54400000000, 'membw_write': 14800000000},
-        'node38': {'membw_read': 255999960000, 'membw_write': 255999960000},
-        'node39': {'membw_read': 255999960000, 'membw_write': 255999960000},
-        'node40': {'membw_read': 33200000000, 'membw_write': 12000000000}
-}
-
 
 def get_mocked_cluster_data_provider():
     mocked_kubeapi = MagicMock(spec=Kubeapi)
@@ -61,7 +37,8 @@ def do_query_side_effect(*args, **kwargs):
     elif args[0] == Prometheus.queries.NODE_CAPACITY_MEM_WSS:
         return create_json_fixture_mock('prometheus_node_capacity_mem_wss').json()['data']['result']
     elif args[0] == Prometheus.queries.NODE_CAPACITY_DRAM_MEMBW:
-        return create_json_fixture_mock('prometheus_node_capacity_dram_membw').json()['data']['result']
+        return create_json_fixture_mock(
+                'prometheus_node_capacity_dram_membw').json()['data']['result']
 
 
 @pytest.mark.parametrize('resources', [
@@ -76,10 +53,41 @@ def test_get_nodes_capacities_raise_exception_no_cpu_or_mem(resources):
         cluster_dp.get_nodes_capacities(resources)
 
 
+CLUSTER_CPU_MEM_CAPACITIES = {
+        'node200': {'cpu': 48.0, 'mem': 201},
+        'node201': {'cpu': 48.0, 'mem': 201},
+        'node202': {'cpu': 48.0, 'mem': 201},
+        'node203': {'cpu': 48.0, 'mem': 201},
+        'node36': {'cpu': 72.0, 'mem': 404},  # k8s master node
+        'node37': {'cpu': 40.0, 'mem': 1064},
+        'node38': {'cpu': 40.0, 'mem': 404},
+        'node39': {'cpu': 40.0, 'mem': 404},
+        'node40': {'cpu': 40.0, 'mem': 1065}
+}
+
+CLUSTER_MEMBW_CAPACITIES = {
+        'node200': {'membw_read': 255999960000, 'membw_write': 255999960000},
+        'node201': {'membw_read': 255999960000, 'membw_write': 255999960000},
+        'node202': {'membw_read': 255999960000, 'membw_write': 255999960000},
+        'node203': {'membw_read': 255999960000, 'membw_write': 255999960000},
+        'node37': {'membw_read': 54400000000, 'membw_write': 14800000000},
+        'node38': {'membw_read': 255999960000, 'membw_write': 255999960000},
+        'node39': {'membw_read': 255999960000, 'membw_write': 255999960000},
+        'node40': {'membw_read': 33200000000, 'membw_write': 12000000000}
+}
+
+CLUSTER_CAPACITIES = {}
+
+for node in CLUSTER_CPU_MEM_CAPACITIES:
+    CLUSTER_CAPACITIES[node] = {
+            **CLUSTER_CPU_MEM_CAPACITIES[node],
+            **CLUSTER_MEMBW_CAPACITIES[node]}
+
+
 @pytest.mark.parametrize('resources, nodes_capacities', [
     ([ResourceType.CPU, ResourceType.MEM], CLUSTER_CPU_MEM_CAPACITIES),
     ([ResourceType.CPU, ResourceType.MEM, ResourceType.MEMBW_READ,
-        ResourceType.MEMBW_WRITE], {**CLUSTER_CPU_MEM_CAPACITIES, **CLUSTER_MEMBW_CAPACITIES}),
+        ResourceType.MEMBW_WRITE], CLUSTER_CAPACITIES),
     ])
 def test_get_nodes_capacities(resources, nodes_capacities):
     cluster_dp = get_mocked_cluster_data_provider()
